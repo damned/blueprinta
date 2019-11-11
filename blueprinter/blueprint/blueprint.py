ac0bd329ad38c065bf8e69a3b4e04d4f99c73eca
damned <writetodan@yahoo.com>
2019-11-11 10:03:40 +0000

    creator defining blueprint

--- blueprinter/blueprint/blueprint.py
+++ blueprinter/blueprint/blueprint.py
@@ -2,13 +2,24 @@ import svgwrite
 
 class Blueprint:
     def __init__(self, filename):
-        self.filename = filename
+        self._svg = svgwrite.Drawing(filename, profile='tiny', size=(800, 600))
+
+    def add_lane(self, name):
+        group = svgwrite.container.Group()
+        self._svg.add(group)
+        return Lane(self._svg, group)
+
 
     def save(self):
-        svg = svgwrite.Drawing(self.filename, profile='tiny', size=(800, 600))
+        self._svg.save()
 
-        group = svgwrite.container.Group()
-        svg.add(group)
 
-        group.add(svg.rect((0, 0), (100, 80), fill='lightblue'))
-        svg.save()
+class Lane:
+    def __init__(self, svg, group):
+        self._group = group
+        self._svg = svg
+
+    def add_card(self, name):
+        self._group.add(self._svg.rect((0, 0), (100, 80), fill='lightblue'))
+
+
--- blueprinter/blueprint_creator.py
+++ blueprinter/blueprint_creator.py
@@ -1,9 +1,12 @@
 from.blueprint.blueprint import Blueprint
 
 class BlueprintCreator:
-    def __init__(self):
-        pass
+    def __init__(self, blueprint_class=Blueprint):
+        self.blueprint_class = blueprint_class
 
     def create_blueprint(self, sheet_filename, blueprint_filename):
-        blueprint = Blueprint(blueprint_filename)
+        blueprint = self.blueprint_class(blueprint_filename)
+        lane = blueprint.add_lane('lane')
+        lane.add_card('card')
+
         blueprint.save()
