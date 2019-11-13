fe292d4f7f1f83bf564914b9554037c727076b6c
damned <writetodan@yahoo.com>
2019-11-13 08:33:41 +0000

    split minimum size

--- blueprinter/blueprint/text_splitter.py
+++ blueprinter/blueprint/text_splitter.py
@@ -1,5 +1,23 @@
 class TextSplitter:
     def split(self, all_text):
         main_text = all_text.split('\n')[0]
-        lines = main_text.split(' ')
+        lines = self.split_after_minimum(main_text)
+        return lines
+
+    def split_after_minimum(self, text, minimum=6):
+        lines = []
+        word = ''
+        for c in text:
+            if c == ' ':
+                if len(word) >= minimum:
+                    lines.append(word)
+                    word = ''
+                else:
+                    word += c
+            else:
+                word += c
+
+        if len(word) > 0:
+            lines.append(word)
+
         return lines
--- test/test_text_splitter.py
+++ test/test_text_splitter.py
@@ -7,7 +7,7 @@ splitter = TextSplitter()
 def test_splits_uneven_words():
     lines = splitter.split('I want bubbles')
 
-    assert lines == ['I', 'want', 'bubbles']
+    assert lines == ['I want', 'bubbles']
 
 def test_splits_even_words_nicely():
     lines = splitter.split('download bubbles ordering form')
